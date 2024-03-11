bbfcec1d9309d71a103bad6791b211c24b083e18
Lfehr199 <landon.fehrenbacher@evsck12.com>
2024-03-10 22:18:18 -0500

    added limit switch

---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
@@ -0,0 +1,97 @@
+package frc.robot.subsystems.Intake;
+
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import frc.robot.subsystems.Uptake.UptakeSubsystem;
+import frc.robot.subsystems.proximity.proximitysubsystem;
+import edu.wpi.first.wpilibj.Timer;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+    
+    private final CANSparkMax centerMotor;
+    private final UptakeSubsystem m_uptake = new UptakeSubsystem();
+    private final proximitysubsystem m_proximity = new proximitysubsystem();
+    public final Timer wait = new Timer();
+    boolean stop;
+    boolean init;
+    double starttime;
+    double timedone;
+    boolean started;
+
+
+
+
+    public IntakeSubsystem() {
+        
+        centerMotor = new CANSparkMax(15 , CANSparkLowLevel.MotorType.kBrushless);
+        centerMotor.restoreFactoryDefaults();
+        centerMotor.setIdleMode(CANSparkMax.IdleMode.kCoast);
+        centerMotor.setInverted(false);
+        wait.reset();
+        init =true;
+        stop=false;
+        started= false;
+        wait.reset();
+        wait.start();
+    }
+
+    public void intake(){
+        centerMotor.set(-.75);
+    }
+
+    public void stop() {
+        centerMotor.set(0);
+    }
+
+    public Command stopIntaking(){
+        return runOnce(() -> {
+           this.stop();
+           this.m_uptake.stop();
+        });
+    }
+
+    public Command startIntaking(){
+       
+        return run(() -> {
+          if (stop == true){
+            if(wait.getFPGATimestamp()>(starttime+1)){
+                stop =false;
+            }
+          }
+           if (m_proximity.pieceInBoolean==false && stop ==false ){
+            this.intake();
+           this.m_uptake.uptake(); 
+           started=false;
+;
+           }
+           else
+           {
+            this.init =false;
+            this.stop();
+            this.m_uptake.stop();
+            if (started==false){
+               started = true;
+               starttime=wait.getFPGATimestamp();
+               stop = true;
+            }
+           }
+        });
+    }
+    
+
+    @Override
+    public void periodic()
+    {
+        //System.out.println("This the position of the intake: " + getIntakePistonPosition());
+        //System.out.println("This is the power of the intake: " + getSpeed());
+
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/proximity/proximitysubsystem.java
@@ -0,0 +1,79 @@
+package frc.robot.subsystems.proximity;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+// import com.revrobotics.Rev2mDistanceSensor;
+// import com.revrobotics.Rev2mDistanceSensor.Port;
+// import com.revrobotics.Rev2mDistanceSensor.RangeProfile;
+
+
+public class proximitysubsystem extends SubsystemBase{
+
+  // private Rev2mDistanceSensor distOnboard; 
+  public Trigger piecein;
+  public Boolean pieceInBoolean=false;
+  DigitalInput toplimitSwitch = new DigitalInput(0);
+
+
+ public proximitysubsystem(){
+    /**
+     * Rev 2m distance sensor can be initialized with the Onboard I2C port
+     * or the MXP port. Both can run simultaneously.
+     */
+    //private distOnboard = new Rev2mDistanceSensor();
+    //distMXP = new Rev2mDistanceSensor(Port.kMXP);
+    // distOnboard = new Rev2mDistanceSensor(Port.kOnboard);
+    // distOnboard.setAutomaticMode(true);
+    piecein = new Trigger(() -> (this.pieceInBoolean));
+  }
+
+ 
+  
+    /**
+     * Before measurements can be read from the sensor, setAutomaticMode(true)
+     * must be called. This starts a background thread which will periodically
+     * poll all enabled sensors and store their measured range.
+     */
+    
+
+    @Override
+    public void periodic(){
+        //System.out.println("This the position of the intake: " + getIntakePistonPosition());
+        //System.out.println("This is the power of the intake: " + getSpeed());
+
+    
+    /**
+     * Range returned from the distance sensor is valid if isRangeValid()
+     * returns true.
+     */
+    // if(distOnboard.isRangeValid()) {
+    //   SmartDashboard.putNumber("Range Onboard", distOnboard.getRange());
+    //   SmartDashboard.putNumber("Timestamp Onboard", distOnboard.getTimestamp());
+    // }
+    // if (distOnboard.getRange() < 3){
+      
+      
+      if (toplimitSwitch.get()) {
+        // We are going up and top limit is tripped so stop
+       pieceInBoolean = true;
+    } else {
+        // We are going up but top limit is not tripped so go at commanded speed
+      pieceInBoolean = false;
+    }
+
+    
+  
+
+ 
+    /**
+     * The background thread may be stopped by calling setAutomaticMode(false).
+     * This will command any active sensors to terminate current measurements
+     * and the thread will stop.
+     */
+    //distOnboard.setAutomaticMode(false);
+    }
+
+    
+}
